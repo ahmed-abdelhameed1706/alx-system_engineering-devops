11f87564d03c4089ccbbb33f113d4b73e8894c21
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-11-07 15:50:43 -0800

    1-top_ten.py

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+function to print top 10
+"""
+
+
+import requests
+
+
+def top_ten(subreddit):
+    """
+    fucntion to query the reddit api
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    r = requests.get(url)
+    if r.status_code < 300:
+        r_json = r.json()
+        children = r_json.get('data').get('children')
+        for child in children:
+            print(child.get('data').get('title'))
+    else:
+        print('None')
