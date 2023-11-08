3e87fd5c511590e525d2e83c80839fae73207da3
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-11-07 18:50:07 -0800

    2-recurse.py

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+function to recursively fetch data
+"""
+import requests
+
+def recurse(subreddit, hot_list=[], after=None):
+    """
+    function to recursivley fetch data
+    """
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+
+    headers = {'User-Agent': 'MyBot'}
+    params = {'limit': 100}
+    if after:
+        params['after'] = after
+
+    res = requests.get(url, headers=headers, params=params, allow_redirects=False)
+
+    if res.status_code == 200:
+
+        data = res.json()['data']
+
+        if data['after'] is not None:
+            recurse(subreddit, hot_list, data['after'])
+
+        for post in data['children']:
+            hot_list.append(post['data']['title'])
+
+        return hot_list
+    else:
+        return None
