76e7d905500157bf8681fbbafee81d5443b47097
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-11-14 04:31:05 +0000

    0-the_sky_is_the_limit_not.pp

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,13 @@
+file { '/etc/default/nginx':
+  ensure  => file,
+  owner   => 'root',
+  group   => 'root',  
+  mode    => '0644',
+  content => "ULIMIT=\"-n 2000\"\n",
+  notify  => Service['nginx']
+}
+
+service { 'nginx':
+  ensure => running,
+  enable => true,
+}
